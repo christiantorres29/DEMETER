69057c4a7edd17ebc567988f1b9fa7d64b4c6630
Christian Torres <christian29catoca@outlook.es>
2023-03-07 15:06:13 -0500

    new videos

--- IMG_TEST/watershed_HPF.py
+++ IMG_TEST/watershed_HPF.py
@@ -4,16 +4,34 @@ from skimage.morphology import disk
 import cv2
 import time
 
-img=cv2.imread("./c3.png")[:,:,2]
+img=cv2.imread("./frame432.png")
+r=cv2.equalizeHist(img[:,:,0])
+g=cv2.equalizeHist(img[:,:,1])
+b=cv2.equalizeHist(img[:,:,2])
 kernel = disk(11)
 kernel2 =disk(1)
+
+#green = np.uint8([0,255,0 ])
+#hsv_green = cv2.cvtColor(green,cv2.COLOR_BGR2HSV)
+
+#hsv = cv2.cvtColor(img, cv2.COLOR_RGB2HSV)
+# define range of green color in HSV
+#lower_green = np.array(green*0.7)
+#upper_green = np.array(green*1.3)
+# Threshold the HSV image to get only green colors
+#mask = cv2.inRange(hsv, lower_green, upper_green)
+# Bitwise-AND mask and original image
+#res = cv.bitwise_and(frame,frame, mask= mask)
+
+
+img=g
 
 start = time.time()
 
 hpf = cv2.adaptiveThreshold(img,255,cv2.ADAPTIVE_THRESH_GAUSSIAN_C,cv2.THRESH_BINARY_INV,61,30)
 #hpf = cv2.adaptiveThreshold(hpf,255,cv2.ADAPTIVE_THRESH_GAUSSIAN_C,cv2.THRESH_BINARY_INV,61,30)
 
-denoised = cv2.medianBlur(hpf,5)
+denoised = hpf#cv2.medianBlur(hpf,5)
 
 #gX = cv2.Sobel(denoised, cv2.CV_64F, 1, 0, ksize=3)
 #gY = cv2.Sobel(denoised, cv2.CV_64F, 0, 1,ksize=3)
@@ -21,6 +39,9 @@ denoised = cv2.medianBlur(hpf,5)
 #gradient = #np.sqrt((gX ** 2) + (gY ** 2)).astype(np.uint8)
 
 #hpf = cv2.GaussianBlur(gradient,(9,9),0)#9 9,21 3, 9 17, 9 3
+
+_,denoised = cv2.threshold(denoised,0,255,cv2.THRESH_BINARY+cv2.THRESH_OTSU)
+
 
 markers = cv2.dilate(denoised,kernel,iterations = 1)
 markers = 255 - cv2.erode(markers,kernel2,iterations=1)
